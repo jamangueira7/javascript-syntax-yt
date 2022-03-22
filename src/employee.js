04fb2381b17ce4e0e96beea70fb0ed0c8bd404fb
João Mangueira <jamjs7@gmail.com>
2022-03-22 19:13:40 -0300

    add alguns testes

--- src/employee.js
+++ src/employee.js
@@ -2,11 +2,16 @@ const EntityBase = require('./entityBase')
 const Util = require('./util')
 
 class Employee extends EntityBase {
-    static #TAXES_PERCENTAL = 0.2
+    static #TAXES_PERCENTUAL = 0.2
     #grosspay = 5000.40
 
     get grossPay() {
         return Util.formatCurrency(this.#grosspay)
+    }
+
+    get netPay() {
+        const result = this.#grosspay - (this.#grosspay * Employee.#TAXES_PERCENTUAL)
+        return Util.formatCurrency(result)
     }
 }
 
